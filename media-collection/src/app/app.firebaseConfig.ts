cdf814b8ec437aa634bae42bf3445438c5bbd53b
aliitani <aliitani0@gmail.com>
2018-10-23 13:25:24 -0400

    changed settings to post public repo

--- media-collection/src/app/app.firebaseConfig.ts
+++ media-collection/src/app/app.firebaseConfig.ts
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
   fire: {
-    apiKey: "AIzaSyA2aRp9MuRxVd-jVwzsxi_KP3fV2HggI2w",
-    authDomain: "media-collection-2a5be.firebaseapp.com",
-    databaseURL: "https://media-collection-2a5be.firebaseio.com",
-    projectId: "media-collection-2a5be",
-    storageBucket: "media-collection-2a5be.appspot.com",
-    messagingSenderId: "839472622194"
+    //  left intentionally blank for the public repo.
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
